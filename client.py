75c9ec0f03c40f3333dd2d55f67d459b09eb07b8
Sunho12 <113418806+Sunho12@users.noreply.github.com>
2023-06-06 00:32:31 +0900

    Update client.py

--- client.py
+++ client.py
@@ -410,11 +410,11 @@ def check_msg(msg, recv_ip):
     
     
     if __name__ == '__main__':
-    SERVER_IP = '127.0.0.1'
-    MY_IP = '127.0.0.1'
-    SERVER_PORT = 12000
-    SIZE = 1024
-    SERVER_ADDR = (SERVER_IP, SERVER_PORT)
+      SERVER_IP = '127.0.0.1'
+      MY_IP = '127.0.0.1'
+      SERVER_PORT = 12000
+      SIZE = 1024
+      SERVER_ADDR = (SERVER_IP, SERVER_PORT)
 
     with socket(AF_INET, SOCK_STREAM) as client_socket:
         client_socket.connect(SERVER_ADDR)
