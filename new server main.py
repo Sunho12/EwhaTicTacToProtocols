5f4ee4c59a504f8e8dd8e943cd1eb13803738485
Sunho12 <113418806+Sunho12@users.noreply.github.com>
2023-06-06 15:12:27 +0900

    Update new server main.py

--- new server main.py
+++ new server main.py
@@ -1,1 +1,27 @@
+if __name__ == '__main__':
 
+    global send_header, recv_header
+    SERVER_PORT = 12000
+    SIZE = 1024
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind(('', SERVER_PORT))
+    server_socket.listen()
+    MY_IP = '127.0.0.1'
+
+    while True:
+        client_socket, client_addr = server_socket.accept()
+
+        start = random.randrange(0,2)
+
+        #send start move information to peer
+        client_socket.send(str(start).encode())
+
+        #receive ack - if ack is correct, start game
+        ack_msg = client_socket.recv(SIZE).decode()
+        if ack_msg == "ACK":
+            root = TTT(client=False, src_addr=MY_IP, dst_addr=client_addr[0])
+            root.play(start_user=start)
+            root.mainloop()
+            client_socket.close()
+            break
+        server_socket.close()
